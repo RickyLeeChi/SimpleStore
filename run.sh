b061e61413b49efee3d5016be0dcea8b1ebec3c0
Ricky Lee <e47380@gmail.com>
2019-07-10 16:45:16 +0800

    update build script

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-java -jar target/simplestore-jar-with-dependencies.jar
+java -cp target/dependency/*:target/*:target/simplestore.jar:. org.sideproject.simplestore.Application
+#java -jar target/simplestore-jar-with-dependencies.jar
